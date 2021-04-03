994b9264a2cae4fc5b811e2b7b6ba42f99880834
Ji-hoon, Lee <zx1056@naver.com>
2021-04-03 15:04:25 +0900

    util code 분리
    날짜 처리 관련

--- src/components/MLineArea.vue
+++ src/components/MLineArea.vue
@@ -141,25 +141,6 @@ export default {
         }
     }
 }
-
-const getFormatDate = (date) => {
-    const coder = (val) => {
-        return val >= 10 ? val : '0' + val
-    }
-
-    const year = date.getFullYear()
-    const month = coder((1 + date.getMonth()))
-    const day = coder(date.getDate())
-    const hour = coder(date.getHours())
-    const minute = coder(date.getMinutes())
-    const second = coder(date.getSeconds())
-    return year + '-' + month + '-' + day +
-        ' ' + hour + ':' + minute + ':' + second
-}
-
-const now = () => {
-    return getFormatDate(new Date())
-}
 </script>
 
 <style>
